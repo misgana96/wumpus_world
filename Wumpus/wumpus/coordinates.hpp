9f79c0ea061e15c212bc43d3ddca1b963c0b23a0
Misgana96 <misganayoseph@gmail.com>
2020-04-09 16:47:08 -0700

    Fix bug on coordinates class

--- Wumpus/wumpus/coordinates.hpp
+++ Wumpus/wumpus/coordinates.hpp
@@ -3,6 +3,27 @@
 #include <cstddef>
 #include <string>
 
+
+/** program_coordinates.cpp ---
+*
+*
+* Authors: Misgana <misganayoseph@gmail.com> and Elias Tsegaw<tsegawelias@gmail.com>
+* Copyright (c) 2020 misgana96
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the  MIT License as
+* published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* MIT License for more details.
+*
+* You should have received a copy of the MIT License
+* along with this program; if not, write to:
+* Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 using namespace std;
 
 enum Directions
